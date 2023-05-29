6e3410005f3be7f7969b2609abfa6a82e26f0075
Nghia <nghia3977@gmail.com>
2023-05-29 11:32:39 +0700

    built categories in home page

--- src/modules/Home/Home.jsx
+++ src/modules/Home/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Categories from "./Categories/Categories";
 
 function Home() {
     return (
-        <div>Home</div>
+        <div>
+            <Categories/>
+        </div>
     );
 }
 
